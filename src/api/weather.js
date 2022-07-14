3bf5cac7ff0b47ffa7a1f82de57a1e8f2ce2450a
crisxchan <chrisgonzales.online@gmail.com>
2022-07-14 20:54:35 +0800

    Fix api leak by using .env

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line consistent-return
 async function getCityWeatherApi(place) {
   try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=a7ac8b5f50476d23defb190a962ac728`, { mode: 'cors' });
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=${process.env.OPEN_WEATHER_KEY}`, { mode: 'cors' });
 
     if (!response.ok) throw new Error('No city found');
     else {
